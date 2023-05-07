af382781e2eac3c92c9f0d80ac1fcbf396649ddd
James Trotter <james@honestbank.com>
2023-05-07 22:22:13 +0700

    feat: initial

--- config/config.go
+++ config/config.go
@@ -18,7 +18,7 @@ type DBConfig struct {
 	DataBase string `default:"weeb" env:"DBNAME"`
 	User     string `default:"weeb" env:"DBUSERNAME"`
 	Password string `required:"true" env:"DBPASSWORD" default:"mysecretpassword"`
-	Port     uint   `default:"3306" env:"DBPort"`
+	Port     uint   `default:"3306" env:"DBPORT"`
 }
 
 func LoadConfigOrPanic() Config {
