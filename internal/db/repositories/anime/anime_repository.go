3e042309ed5927b493136efff262968583b509c1
James <admin@floretos.com>
2024-07-06 16:29:23 -0400

    fix(currently-aring): show only animes that have not finished airing

--- internal/db/repositories/anime/anime_repository.go
+++ internal/db/repositories/anime/anime_repository.go
@@ -644,7 +644,7 @@ func (a *AnimeRepository) AiringAnime(ctx context.Context, limit int) ([]*Anime,
 		Joins("JOIN (?) e ON anime.id = e.anime_id", subQuery).
 		Where("anime.status = ?", "Finished Airing").
 		Where("anime.start_date < ?", time.Now()).
-		Where("anime.end_date > ?", time.Now()).
+		Where("anime.end_date IS NULL OR anime.end_date > ?", time.Now()).
 		Order("e.aired").
 		Scan(&animes).Error
 
