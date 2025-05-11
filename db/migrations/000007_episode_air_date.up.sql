be4ff6a39da2cbf622a4d6690d9f8b9365628f81
James <admin@floretos.com>
2025-05-11 08:56:48 -0400

    fix: migration

--- db/migrations/000007_episode_air_date.up.sql
+++ db/migrations/000007_episode_air_date.up.sql
@@ -1,10 +1,3 @@
--- Rename the original 'aired' column
 ALTER TABLE episodes RENAME COLUMN aired TO backup_aired;
-
--- Add the new 'aired' column as DATE
 ALTER TABLE episodes ADD COLUMN aired DATE;
-
--- Set 'aired' only where 'backup_aired' is not null
-UPDATE episodes
-SET aired = STR_TO_DATE(backup_aired, '%Y-%m-%d %H:%i:%s')
-WHERE backup_aired IS NOT NULL;
+UPDATE episodes SET aired = STR_TO_DATE(backup_aired, '%Y-%m-%d %H:%i:%s') WHERE backup_aired IS NOT NULL;
