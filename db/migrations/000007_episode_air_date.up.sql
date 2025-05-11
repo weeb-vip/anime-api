dee7d0010f490ebce2f5e26f18a3caec5543c53a
James <admin@floretos.com>
2025-05-11 08:41:52 -0400

    fix: migration

--- db/migrations/000007_episode_air_date.up.sql
+++ db/migrations/000007_episode_air_date.up.sql
@@ -4,4 +4,6 @@ ALTER TABLE episodes RENAME COLUMN aired TO backup_aired;
 ALTER TABLE episodes ADD COLUMN aired date;
 
 /* update aired column with the value from backup_aired */
-UPDATE episodes SET aired = to_timestamp(backup_aired, 'YYYY-MM-DD HH24:MI:SS')::date;
+UPDATE episodes
+SET aired = to_timestamp(backup_aired, 'YYYY-MM-DD HH24:MI:SS')::date
+WHERE backup_aired IS NOT NULL;
