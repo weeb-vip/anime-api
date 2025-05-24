5fa1ebde91c4cd104164da061857e523c83f4415
James <admin@floretos.com>
2025-05-24 16:39:38 -0400

    fix: query

--- internal/db/repositories/anime_character/anime_character_repository.go
+++ internal/db/repositories/anime_character/anime_character_repository.go
@@ -25,14 +25,14 @@ func NewAnimeCharacterRepository(db *db.DB) AnimeCharacterRepositoryImpl {
 }
 
 func (a *AnimeCharacterRepository) FindAnimeCharacterAndStaffByAnimeId(ctx context.Context, animeId string) ([]*AnimeCharacterWithStaff, error) {
-	var animeCharacters []*AnimeCharacterWithStaff
+	var animeCharactersWithStaff []*AnimeCharacterWithStaff
 	// use anime_character_staff_link table to join anime_character and anime_staff
 	err := a.db.DB.Table("anime_character").
-		Select("anime_character.*, anime_staff.*").
+		Select("anime_character.*, anime_staff.given_name, anime_staff.family_name, anime_staff.birth_place, anime_staff.blood_type, anime_staff.hobbies").
 		Joins("JOIN anime_character_staff_link ON anime_character.id = anime_character_staff_link.character_id").
 		Joins("JOIN anime_staff ON anime_character_staff_link.staff_id = anime_staff.id").
 		Where("anime_character.anime_id = ?", animeId).
-		Scan(&animeCharacters).Error
+		Scan(&animeCharactersWithStaff).Error
 	if err != nil {
 		return nil, err
 	}
